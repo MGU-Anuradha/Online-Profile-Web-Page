6794c53d36395a8c43567a138bc9c7fbafcde25b
Asus <anuradh-se19049@stu.kln.ac.lk>
2023-08-07 16:10:10 +0530

    Did color changes

--- js/script.js
+++ js/script.js
@@ -11,14 +11,32 @@ $(document).ready(function(){
     });
 });  
 
-
+ 
 // JavaScript function to toggle academic and curriculum content visibility
 function toggleContent(contentId) {
     var content = document.getElementById(contentId);
-    if (content.classList.contains('appear')) {
-        content.classList.remove('appear')
+    var contentBox = content.closest('.achievement-box');
+    
+    if (contentBox.classList.contains('active')) {
+      // Hide the content box
+      contentBox.classList.remove('active');
+      content.style.maxHeight = '0';
+    } else {
+      // Show the content box
+      contentBox.classList.add('active');
+      content.style.maxHeight = '600px';
     }
-    else {
-        content.classList.add('appear');
-    }
-}
+  }
+
+//   // JavaScript for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  
